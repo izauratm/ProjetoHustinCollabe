8af81a3ae1e66c74a91f1a7a1afcf01ef561ac56
Izauratm <izauratm@gmail.com>
2025-05-01 23:34:42 -0300

    Versão Projeto HustinCollabe

---
+++ hustincollabe/hustincollabe/src/test/java/com/senac/hustincollabe/HustincollabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.hustincollabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustincollabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
